4ac703fa21673ea5065cccf9300a66b5f2a87678
Brajananda Mahata <brajanandamahato280603@gmail.com>
2024-11-24 19:07:46 +0530

    4 power of a number

---
+++ 4_power_of_a_number/power_of_number.py
@@ -0,0 +1,6 @@
+number = int(input("enter the number : "))
+
+exponent = int(input("enter the power : "))
+
+print(str(number) + " to the power " + str(exponent) + " is equal to " + str(number**exponent))
+
